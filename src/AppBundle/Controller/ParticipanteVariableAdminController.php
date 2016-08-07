ccf248eb354f5711a5089a3a43c576f29adcf309
odeg36 <odeg36@hotmail.com>
2016-08-07 17:45:24 -0500

    Mes

--- src/AppBundle/Controller/ParticipanteVariableAdminController.php
+++ src/AppBundle/Controller/ParticipanteVariableAdminController.php
@@ -33,7 +33,49 @@ class ParticipanteVariableAdminController extends Controller
 			$objWorksheet->setCellValue('A'.$fila, $registro->getId());
 			$objWorksheet->setCellValue('B'.$fila, $registro->getParticipante()->getIdentificador());
 			$objWorksheet->setCellValue('C'.$fila, $registro->getParticipante()->getNombre());
-			$objWorksheet->setCellValue('D'.$fila, $registro->getMes());
+			$mes = "";
+			switch ($registro->getMes()) {
+				case 1:
+					$mes = "Enero";
+					break;
+				case 2:
+					$mes = "Febrero";
+					break;
+				case 3:
+					$mes = "Marzo";
+					break;
+				case 4:
+					$mes = "Abril";
+					break;
+				case 5:
+					$mes = "Mayo";
+					break;
+				case 6:
+					$mes = "Junio";
+					break;
+				case 7:
+					$mes = "Julio";
+					break;
+				case 8:
+					$mes = "Agosto";
+					break;
+				case 9:
+					$mes = "Septiembre";
+					break;
+				case 10:
+					$mes = "Octubre";
+					break;
+				case 11:
+					$mes = "Noviembre";
+					break;
+				case 12:
+					$mes = "Diciembre";
+					break;
+				default:
+					# code...
+					break;
+			}
+			$objWorksheet->setCellValue('D'.$fila, $mes);
 			$objWorksheet->setCellValue('E'.$fila, $registro->getPorcentajeCumplimiento()."%");
 			$objWorksheet->setCellValue('F'.$fila, $registro->getPorcentajeCumplimiento()."%");
 
